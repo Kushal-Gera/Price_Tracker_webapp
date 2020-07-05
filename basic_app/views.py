c7b421338497e054639dac644423ef7659879bd2
KUSHAL <kushalgera1212@gamil.com>
2020-07-05 16:21:51 +0530

    firebase added for modals and fields

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, redirect
 from basic_app.forms import ProductForm
 from django.contrib import messages
+from firebase import firebase
 
 import requests
 from bs4 import BeautifulSoup
 
 # Create your views here.
+def saved(request):
+    return render(request, "basic_app/saved.html", {})
+
+
 def index(request):
 
     if request.POST:
         form = ProductForm(request.POST)
         if form.is_valid() and prod_valid(request.POST["product_url"]):
-            form.save()
+            # form.save()
+            save_to_fb(request.POST)
             return redirect("/saved/")
         else:
             messages.info(request, "Invalid Product Link !")
@@ -21,19 +27,33 @@ def index(request):
         return render(request, "basic_app/index.html", {"form":form})
 
 
-def saved(request):
-    return render(request, "basic_app/saved.html", {})
+def save_to_fb(data_dict):
+    fb = firebase.FirebaseApplication("https://barcode-scanner-92b37.firebaseio.com/", None)
+    data = {
+        "product_name" : data_dict["product_name"],
+        "product_url" : data_dict["product_url"],
+        "target_price" : data_dict["target_price"],
+        "your_email" : data_dict["your_email"]
+    }
+
+    fb.post("/Products/", data)
+    print("saved data !!")
 
 
 def prod_valid(url):
-    HEADERS = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+    HEADERS = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
 
     data = requests.get(url, headers=HEADERS).text
     soup = BeautifulSoup(data, "html.parser")
 
-    title = soup.find(id="productTitle").get_text().strip()
+    title_item = soup.find(id="productTitle")
+    title = ""
 
-    return title != None
+    if title_item:
+     title = title_item.get_text().strip()
+
+    print(title)
+    return len(title) > 0
 
 
 
